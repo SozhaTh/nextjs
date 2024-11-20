d306be52ccb08e928675cceefb3580e443da6d1c
SozhaTh <sozhaadonay@gmail.com>
2024-11-20 18:48:41 -0500

    bd

--- nextjs-vercel/src/libs/mysql.js
+++ nextjs-vercel/src/libs/mysql.js
@@ -1,12 +1,11 @@
 import mysql from 'serverless-mysql';
 
 export const conn = mysql({
-    config: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        port: 3306,
-        database: 'prueba'
-    }
+  config: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'prueba',
+    port: process.env.DB_PORT || 3306,
+  },
 });
-
